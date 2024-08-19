de574a0a40d03e1d1464db92d55f9a1e004d1615
sea <666666@qq.com>
2024-08-19 10:01:11 +0800

    1

--- build/start1.sh
+++ build/start1.sh
@@ -2,6 +2,6 @@
 
 pkill -9 geth
 
-bin/geth --datadir node1 --port "30311" --http --http.addr "127.0.0.1" --http.port "8501" --http.api "personal,eth,admin,net,web3,txpool,miner" --networkid 168  --miner.gasprice "0" --allow-insecure-unlock --unlock "0x607837901C98d9820230387a844Dec52570DbE4e" --password node1/password.txt --miner.etherbase "0x607837901C98d9820230387a844Dec52570DbE4e" --mine >node1.log 2>&1 &
+bin/geth --datadir node1 --port "30311" --ws --ws.addr "127.0.0.1" --ws.port "8601" --http --http.addr "127.0.0.1" --http.port "8501" --http.api "personal,eth,admin,net,web3,txpool,miner" --networkid 168  --miner.gasprice "0" --allow-insecure-unlock --unlock "0x607837901C98d9820230387a844Dec52570DbE4e" --password node1/password.txt --miner.etherbase "0x607837901C98d9820230387a844Dec52570DbE4e" --mine >node1.log 2>&1 &
 
 tail -f node1.log
--- build/start2.sh
+++ build/start2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bin/geth --datadir "node2" --port "30312" --http --http.addr "127.0.0.1" --http.port "8502" --http.api "personal,eth,admin,net,web3,txpool,miner" --networkid 168 --miner.gasprice "0" --allow-insecure-unlock --unlock "0x4E380a4030fbC6B85aCd022eE75932d902c9243c" --password node2/password.txt --miner.etherbase "0x4E380a4030fbC6B85aCd022eE75932d902c9243c" --mine  --bootnodes "enode://848706b535802c61bc971b663f4ff020202587f34d87be938b19f15de20d70c1d5c776aa335694dabf3a6cc1ee00f48ca0541f91353e275e08a6520587b7d036@127.0.0.1:30311" >node2.log 2>&1 &
+bin/geth --datadir "node2" --port "30312" --ws --ws.addr "0.0.0.0" --ws.port "8602" --http --http.addr "0.0.0.0" --http.port "8502" --http.api "personal,eth,admin,net,web3,txpool,miner" --networkid 168 --miner.gasprice "0" --allow-insecure-unlock --unlock "0x4E380a4030fbC6B85aCd022eE75932d902c9243c" --password node2/password.txt --miner.etherbase "0x4E380a4030fbC6B85aCd022eE75932d902c9243c" --mine  --bootnodes "enode://848706b535802c61bc971b663f4ff020202587f34d87be938b19f15de20d70c1d5c776aa335694dabf3a6cc1ee00f48ca0541f91353e275e08a6520587b7d036@127.0.0.1:30311" >node2.log 2>&1 &
 
 tail -f node2.log
