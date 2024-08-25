15059d1ae29169770e54452b9f524a129907ad99
sea <666666@qq.com>
2024-08-25 12:44:01 +0800

    1

--- build/start.sh
+++ build/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bin/geth --datadir "node3" --port "30316" --ws --ws.addr "0.0.0.0" --ws.port "8646" --http --http.addr "0.0.0.0" --http.port "8645" --http.api "personal,eth,admin,net,web3,txpool,miner,debug" --networkid 168 --miner.gasprice "0" --allow-insecure-unlock --unlock "0x0aEA566959ff98c3BcfaD4F732Bb3C8388fD87EB" --password node3/password.txt --miner.etherbase "0x0aEA566959ff98c3BcfaD4F732Bb3C8388fD87EB" --mine  --bootnodes "enode://bfb2f6a2aaa76ef95624906508b2151ab316a2b7e6928f7b248e7e50f4db2326c9fca70ee789f757946dc090884fdd9a0c35ec431b19dc9cd21ef856ed8bf7ec@127.0.0.1:30311" >node3.log 2>&1 &
+bin/geth --datadir "node3" --port "30316" --ws --ws.addr "0.0.0.0" --ws.port "8646" --http --http.addr "0.0.0.0" --http.port "8645" --http.api "personal,eth,admin,net,web3,txpool,miner,debug" --networkid 69916 --miner.gasprice "0" --allow-insecure-unlock --unlock "0x0aEA566959ff98c3BcfaD4F732Bb3C8388fD87EB" --password node3/password.txt --miner.etherbase "0x0aEA566959ff98c3BcfaD4F732Bb3C8388fD87EB" --mine  --bootnodes "enode://bfb2f6a2aaa76ef95624906508b2151ab316a2b7e6928f7b248e7e50f4db2326c9fca70ee789f757946dc090884fdd9a0c35ec431b19dc9cd21ef856ed8bf7ec@127.0.0.1:30311" >node3.log 2>&1 &
 
 tail -f node3.log
