3ec9a9c68a66cbccf5279c08e914046e8e230ec2
sea <666666@qq.com>
2025-07-02 10:26:02 +0800

    a

--- build/build-genesis.sh
+++ build/build-genesis.sh
@@ -2,6 +2,4 @@
 
 pkill -9 geth
 rm -rf node1/geth
-rm -rf node2/geth
 bin/geth --datadir node1 init genesis.json
-bin/geth --datadir node2 init genesis.json
--- build/start1.sh
+++ build/start1.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-pkill -9 geth
-
 bin/geth  --syncmode "full" --datadir "node1" --port "30316" --ws --ws.addr "0.0.0.0" --ws.port "8646" --http --http.addr "0.0.0.0" --http.port "8645" --http.api "personal,eth,admin,net,web3,txpool,miner,debug" --networkid 73619 --miner.gasprice "0" --allow-insecure-unlock --unlock "0xe83E51a1912bd83E456568c275D933BF3A8B95c6" --password node1/password.txt --miner.etherbase "0xe83E51a1912bd83E456568c275D933BF3A8B95c6" --mine  >node1.log 2>&1 &
 
 tail -f node1.log
