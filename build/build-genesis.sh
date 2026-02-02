8cad36c28193c01823d718796a414d6fa96db703
sea <666666@qq.com>
2026-02-02 17:11:30 +0800

    a

--- build/build-genesis.sh
+++ build/build-genesis.sh
@@ -3,5 +3,7 @@
 pkill -9 geth
 rm -rf node1/geth
 rm -rf node2/geth
+rm -rf node3/geth
 bin/geth --datadir node1 init genesis.json
 bin/geth --datadir node2 init genesis.json
+bin/geth --datadir node3 init genesis.json
--- build/start2.sh
+++ build/start2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bin/geth --datadir "node2" --port "30312" --ws --ws.addr "0.0.0.0" --ws.port "8602" --http --http.addr "0.0.0.0" --http.port "8502" --http.api "personal,eth,admin,net,web3,txpool,miner,debug" --networkid 168 --miner.gasprice "0" --allow-insecure-unlock --unlock "0x4E380a4030fbC6B85aCd022eE75932d902c9243c" --password node2/password.txt --miner.etherbase "0x4E380a4030fbC6B85aCd022eE75932d902c9243c" --mine  --bootnodes "enode://bfb2f6a2aaa76ef95624906508b2151ab316a2b7e6928f7b248e7e50f4db2326c9fca70ee789f757946dc090884fdd9a0c35ec431b19dc9cd21ef856ed8bf7ec@127.0.0.1:30311" >node2.log 2>&1 &
+bin/geth --datadir "node2" --port "30312" --ws --ws.addr "0.0.0.0" --ws.port "8602" --http --http.addr "0.0.0.0" --http.port "8502" --http.api "personal,eth,admin,net,web3,txpool,miner,debug" --networkid 168 --miner.gasprice "0" --allow-insecure-unlock --unlock "0x4E380a4030fbC6B85aCd022eE75932d902c9243c" --password node2/password.txt --miner.etherbase "0x4E380a4030fbC6B85aCd022eE75932d902c9243c" --mine  --bootnodes "enode://b430568144f24e480687b411493112da5b7de8863bff5276bbcf2b333c8291b85d58efa2c6bccc61f59eb7e6f51189016c4afe7e66cd92c799a225017bf59ea3@127.0.0.1:30311" >node2.log 2>&1 &
 
 tail -f node2.log
