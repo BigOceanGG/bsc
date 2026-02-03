e4104e11b41338b39cac26216834d910bd38e1d7
sea <666666@qq.com>
2026-02-03 13:46:36 +0800

    a

--- build/start2.sh
+++ build/start2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bin/geth --datadir "node2" --port "30312" --ws --ws.addr "0.0.0.0" --ws.port "8602" --http --http.addr "0.0.0.0" --http.port "8502" --http.api "personal,eth,admin,net,web3,txpool,miner,debug" --networkid 168 --miner.gasprice "0" --allow-insecure-unlock --unlock "0x4E380a4030fbC6B85aCd022eE75932d902c9243c" --password node2/password.txt --miner.etherbase "0x4E380a4030fbC6B85aCd022eE75932d902c9243c" --mine  --bootnodes "enode://b430568144f24e480687b411493112da5b7de8863bff5276bbcf2b333c8291b85d58efa2c6bccc61f59eb7e6f51189016c4afe7e66cd92c799a225017bf59ea3@127.0.0.1:30311" >node2.log 2>&1 &
+bin/geth --datadir "node2" --port "30312" --ws --ws.addr "0.0.0.0" --ws.port "8602" --http --http.addr "0.0.0.0" --http.port "8102" --http.api "personal,eth,admin,net,web3,txpool,miner,debug" --networkid 69916 --miner.gasprice "0" --allow-insecure-unlock --unlock "0x4E380a4030fbC6B85aCd022eE75932d902c9243c" --password node2/password.txt --miner.etherbase "0x4E380a4030fbC6B85aCd022eE75932d902c9243c" --mine  --bootnodes "enode://dc3e2af489bf4d21a50f7f38727971613aa5b33a697c46a42eb83a3df313ee5e6979017bdd7a77e8013b344fda9fa2bcb0c2e9c826cee304795210238031e755@127.0.0.1:30311" >node2.log 2>&1 &
 
 tail -f node2.log
--- build/start3.sh
+++ build/start3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bin/geth --datadir "node3" --port "30313" --ws --ws.addr "0.0.0.0" --ws.port "8603" --http --http.addr "0.0.0.0" --http.port "8503" --http.api "personal,eth,admin,net,web3,txpool,miner,debug" --networkid 168 --miner.gasprice "0" --allow-insecure-unlock --unlock "0x0aEA566959ff98c3BcfaD4F732Bb3C8388fD87EB" --password node3/password.txt --miner.etherbase "0x0aEA566959ff98c3BcfaD4F732Bb3C8388fD87EB" --mine  --bootnodes "enode://b430568144f24e480687b411493112da5b7de8863bff5276bbcf2b333c8291b85d58efa2c6bccc61f59eb7e6f51189016c4afe7e66cd92c799a225017bf59ea3@127.0.0.1:30311" >node3.log 2>&1 &
+bin/geth --datadir "node3" --port "30313" --ws --ws.addr "0.0.0.0" --ws.port "8603" --http --http.addr "0.0.0.0" --http.port "8103" --http.api "personal,eth,admin,net,web3,txpool,miner,debug" --networkid 69916 --miner.gasprice "0" --allow-insecure-unlock --unlock "0x0aEA566959ff98c3BcfaD4F732Bb3C8388fD87EB" --password node3/password.txt --miner.etherbase "0x0aEA566959ff98c3BcfaD4F732Bb3C8388fD87EB" --mine  --bootnodes "enode://dc3e2af489bf4d21a50f7f38727971613aa5b33a697c46a42eb83a3df313ee5e6979017bdd7a77e8013b344fda9fa2bcb0c2e9c826cee304795210238031e755@127.0.0.1:30311" >node3.log 2>&1 &
 
 tail -f node3.log
